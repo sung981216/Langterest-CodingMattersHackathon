e997a81c06d0e4cf674e68b92d3f5ddba7de9ea1
Sung Na <sung981216@gmail.com>
2021-02-14 16:04:28 -0800

    Got firebase cloudstore to set up and users can make a post about Langley

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { dbService } from "fbase";
 
 const Home = () => {
-  return <span>home</span>;
+  const [interest, setPost] = useState("");
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection("interest").add({
+      interest,
+      createdAt: Date.now(),
+    });
+    setPost("");
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setPost(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={interest}
+          onChange={onChange}
+          placeholder="What's going on in Langley?"
+          maxLength={120}
+        />
+        <input type="submit" value="Post" />
+      </form>
+    </div>
+  );
 };
 
 export default Home;
